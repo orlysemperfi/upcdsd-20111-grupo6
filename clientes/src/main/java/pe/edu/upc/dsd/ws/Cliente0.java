3c5ac65a68952f61bcfe76f6837b652b056f163e
alejandro.murillo.1988@gmail.com <alejandro.murillo.1988@gmail.com@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-05-17 01:20:42 +0000

    Finalcasi

---
+++ clientes/src/main/java/pe/edu/upc/dsd/ws/Cliente0.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.dsd.ws;
+
+public class Cliente0 {
+	
+	private int codigo;
+	
+	private String nombre;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+
+}
