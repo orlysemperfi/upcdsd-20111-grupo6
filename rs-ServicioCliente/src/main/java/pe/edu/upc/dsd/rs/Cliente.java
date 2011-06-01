ebe94eab2086d0cf0cb4a4324b6fc1af932c53d9
jrufasto5 <jrufasto5@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-06-01 22:12:18 +0000

    clase cliente actualizando...

---
+++ rs-ServicioCliente/src/main/java/pe/edu/upc/dsd/rs/Cliente.java
@@ -0,0 +1,65 @@
+package pe.edu.upc.dsd.rs;
+
+public class Cliente {
+	
+	private String codigo;
+	private String nombre;
+	private String apellido;
+	private String ciudad;
+	private String distrito;
+	private String direccion;
+	private String dni;
+	private String telefono;
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getDistrito() {
+		return distrito;
+	}
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+
+}
+
