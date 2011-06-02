0cc2e32d439a7482b8f834d651bdc66e476fb4df
jrufasto5 <jrufasto5@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-06-02 22:29:37 +0000

    actualizando serviciocliente

--- rs-ServicioCliente/src/main/java/pe/edu/upc/dsd/rs/ClienteService.java
+++ rs-ServicioCliente/src/main/java/pe/edu/upc/dsd/rs/ClienteService.java
@@ -1,18 +1,24 @@
 package pe.edu.upc.dsd.rs;
 import javax.ws.rs.GET;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.google.gson.Gson;
 
+@Path ("/cliente")
 public class ClienteService {
 	
 	
-	@GET
+	/*@GET
 	@Path ("/cliente/{codigo}")
-	@Produces("application/json")
+	@Produces("application/json")*/
 	
+	
+	@GET 
+    @Path("/consulta/{codigo}") 
+    @Produces("application/json")//("text/plain") 
 	public String getCliente (@PathParam("codigo") String codigo){
 		Gson gson = new Gson();
 		Cliente cliente = new Cliente();
@@ -24,30 +30,36 @@ public class ClienteService {
 		cliente.setDireccion("Av. Nueva York");
 		cliente.setDni("4904994");
 		cliente.setTelefono("99441514");
-		
-		
-		
 		return gson.toJson(codigo); 
 	}
+
+
+		@GET 
+		@Path("/listar/{nombre}") 
+		@Produces("text/plain") 
+		public String listar(@PathParam("nombre") String nombre) { 
+		        return "Bienvenido : " + nombre; 
+		} 
 	
-	/*@GET
-	@Path ("/cliente/{ciudad}")
-	@Produces("application/text")
-	public String getClienteCiudad (@PathParam("ciudad") String ciudad){
-		Gson gson = new Gson();
-		Cliente cliente = new Cliente();
-		cliente.setCodigo("C0001");
-		cliente.setNombre("Junior");
-		cliente.setApellido("Rufasto");
-		cliente.setCiudad("Lima");
-		cliente.setDistrito("Breña");
-		cliente.setDireccion("Av. Nueva York");
-		cliente.setDni("4904994");
-		cliente.setTelefono("99441514");
-		
-		
-		
-		return gson.toJson(ciudad); 
-	}*/
+	/*@Path("/HelloWorld") 
+	public class HelloWorld { 
+	 
+	        @GET 
+	        @Path("/saludar") 
+	        @Produces("text/plain") 
+	        public String saludar() { 
+	                return "Hola mundo!!!"; 
+	        } 
+	 
+	        @GET 
+	        @Path("/saludar/{nombre}") 
+	        @Produces("text/plain") 
+	        public String saludar(@PathParam("nombre") String nombre) { 
+	                return "Hola " + nombre; 
+	        } 
+	 
+	} */
+
+	
 
 }
