1f2e172873ffb25456ce3e18dfffefcbac0000ec
jrufasto5 <jrufasto5@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-06-02 19:29:32 +0000

    importando clases consultaCLiente

---
+++ ConsultaCliente/src/test/java/pe/edu/upc/dsd/rs/consultarCliente.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.dsd.rs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import junit.framework.Assert;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+public class consultarCliente {
+	@Test
+	public void test() throws Exception {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(
+				"http://localhost:8080/rs-ServicioCliente/rest/cliente/C0001");
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				entity.getContent()));
+		Assert.assertEquals("Cliente encontrado", reader.readLine());
+	}
+	
+	
+}
