b4110ba415c7cdb788b48ada2feb1f21c11c9c66
alejandro.murillo.1988@gmail.com <alejandro.murillo.1988@gmail.com@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-06-16 05:04:36 +0000

    Final

---
+++ trunk/ConsultaCliente4/src/test/java/pe/edu/upc/dsd/rs/listarRutas.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.dsd.rs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import junit.framework.Assert;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+public class listarRutas {
+	@Test
+	public void test() throws Exception {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(
+				"http://localhost:8080/rs-ServicioRuta/rest/ruta/listar/Arequipa");
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				entity.getContent()));
+		Assert.assertEquals("Arequipa", reader.readLine());
+	}
+}
