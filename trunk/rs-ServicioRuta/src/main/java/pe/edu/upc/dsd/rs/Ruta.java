1ec913f87c55954ce0e66ae5871e762194426898
alejandro.murillo.1988@gmail.com <alejandro.murillo.1988@gmail.com@21df4d1a-75a9-be8d-b2e1-0b7d815db94b>
2011-06-16 03:40:37 +0000

    Servidor Finalizado Rutas

---
+++ trunk/rs-ServicioRuta/src/main/java/pe/edu/upc/dsd/rs/Ruta.java
@@ -0,0 +1,128 @@
+package pe.edu.upc.dsd.rs;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Cliente")
+public class Ruta {
+	
+	private String codbus;
+	private String codruta;
+	private String salida;
+	private String llegada;
+	private String fechasalida;
+	private String fechallegada;
+	private String horasalida;
+	private String horallegada;
+	private String conductor;
+	private String costo;
+	private String asientos;
+	
+	public Ruta(){}
+	
+	public Ruta(String codbus, String codruta,String salida,String llegada, String fechasalida,
+			String fechallegada, String horasalida, String horallegada, String conductor,
+			String costo,String asientos) {
+		super();
+		this.codbus = codbus;
+		this.codruta = codruta;
+		this.salida = salida;
+		this.llegada = llegada;
+		this.fechasalida = fechasalida;
+		this.fechallegada = fechallegada;
+		this.horasalida = horasalida;
+		this.horallegada = horallegada;
+		this.conductor = conductor;
+		this.costo = costo;
+		this.asientos = asientos;
+	}
+
+	public String getSalida() {
+		return salida;
+	}
+
+	public void setSalida(String salida) {
+		this.salida = salida;
+	}
+
+	public String getLlegada() {
+		return llegada;
+	}
+
+	public void setLlegada(String llegada) {
+		this.llegada = llegada;
+	}
+
+	public String getCodbus() {
+		return codbus;
+	}
+
+	public void setCodbus(String codbus) {
+		this.codbus = codbus;
+	}
+
+	public String getCodruta() {
+		return codruta;
+	}
+
+	public void setCodruta(String codruta) {
+		this.codruta = codruta;
+	}
+
+	public String getFechasalida() {
+		return fechasalida;
+	}
+
+	public void setFechasalida(String fechasalida) {
+		this.fechasalida = fechasalida;
+	}
+
+	public String getFechallegada() {
+		return fechallegada;
+	}
+
+	public void setFechallegada(String fechallegada) {
+		this.fechallegada = fechallegada;
+	}
+
+	public String getHorasalida() {
+		return horasalida;
+	}
+
+	public void setHorasalida(String horasalida) {
+		this.horasalida = horasalida;
+	}
+
+	public String getHorallegada() {
+		return horallegada;
+	}
+
+	public void setHorallegada(String horallegada) {
+		this.horallegada = horallegada;
+	}
+
+	public String getConductor() {
+		return conductor;
+	}
+
+	public void setConductor(String conductor) {
+		this.conductor = conductor;
+	}
+
+	public String getCosto() {
+		return costo;
+	}
+
+	public void setCosto(String costo) {
+		this.costo = costo;
+	}
+
+	public String getAsientos() {
+		return asientos;
+	}
+
+	public void setAsientos(String asientos) {
+		this.asientos = asientos;
+	}
+}
+
+	
---
+++ trunk/rs-ServicioRuta/src/main/java/pe/edu/upc/dsd/rs/RutaService.java
@@ -0,0 +1,111 @@
+package pe.edu.upc.dsd.rs;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import com.google.gson.Gson;
+import org.apache.log4j.Logger;
+
+
+
+@Path ("/ruta")
+public class RutaService {
+	
+	private static final Logger logger = Logger
+	.getLogger(RutaService.class);
+	/*@GET
+	@Path ("/cliente/{codigo}")
+	@Produces("application/json")*/
+	
+	
+	@GET 
+	    @Path("/consulta/{destino}") 
+	    @Produces("application/json")//("text/plain") 
+		public String getCliente (@PathParam("destino") String destino){
+			Gson gson = new Gson();
+			Ruta ruta = new Ruta();
+ruta.setCodbus("B001");
+ruta.setCodruta("R001");
+ruta.setSalida("Arequipa");
+ruta.setLlegada("Lima");
+ruta.setCodruta("R001");
+ruta.setFechasalida("19/04/2010");
+ruta.setFechallegada("19/04/2010");
+ruta.setHorasalida("18:00:00");
+ruta.setHorallegada("06:00:00");
+ruta.setConductor("Mario Perez");
+ruta.setCosto("13 Soles");
+ruta.setAsientos("29");
+
+			//System.out.println("CLIENTE: C0001 ");
+			logger.debug("Destino: " + destino);
+			return gson.toJson(ruta); 
+		}
+	
+	
+	@GET 
+    @Path("/consulta2/{destino}/{llegada}/{horasalida}") 
+    @Produces("application/json")
+	public String getClientes (@PathParam("destino") String destino, @PathParam("llegada") String llegada, @PathParam("horasalida") String horasalida) {
+		Gson gson = new Gson();
+		Ruta ruta = new Ruta();
+		ruta.setCodbus("B001");
+		ruta.setCodruta("R001");
+		ruta.setSalida("Arequipa");
+		ruta.setLlegada("Lima");
+		ruta.setFechasalida("19/04/2010");
+		ruta.setFechallegada("19/04/2010");
+		ruta.setHorasalida("18:00:00");
+		ruta.setHorallegada("06:00:00");
+		ruta.setConductor("Mario Perez");
+		ruta.setCosto("13 Soles");
+		ruta.setAsientos("29");
+
+		//System.out.println("CLIENTE: C0001 ");
+		logger.debug("Consulta Rutas: " + destino + " " + llegada + " " + horasalida );
+		return gson.toJson(ruta); 
+	}
+	
+	@GET 
+    @Path("/consulta3/{destino}/{llegada}/{horasalida}") 
+    @Produces("application/json")
+	public List<Ruta> getListaClientes (@PathParam("destino") String destino, @PathParam("llegada") String llegada, @PathParam("horasalida") String horasalida){
+		
+		List<Ruta> lista = new ArrayList<Ruta>();
+		Ruta ruta = new Ruta();
+		ruta.setCodbus("B001");
+		ruta.setCodruta("R001");
+		ruta.setSalida("Arequipa");
+		ruta.setLlegada("Lima");
+		ruta.setFechasalida("19/04/2010");
+		ruta.setFechallegada("19/04/2010");
+		ruta.setHorasalida("18:00:00");
+		ruta.setHorallegada("06:00:00");
+		ruta.setConductor("Mario Perez");
+		ruta.setCosto("13 Soles");
+		ruta.setAsientos("29");
+		lista.add(ruta);
+		lista.add(new Ruta("B002", "R002", "Arequipa", "Lima", "19/04/2010", "19/04/2010", "19:00:00", "22:00:00" ,"Mario Perez", "19 Soles", "26"));
+		lista.add(new Ruta("B003", "R003", "Lima", "Arequipa", "20/04/2010", "22/04/2010", "21:00:00","06:00:00", "Mario Perez", "18 Soles", "25"));
+		lista.add(new Ruta("B004", "R004", "Cuzco", "Lima", "21/04/2010", "22/04/2010", "22:00:00", "03:00:00","Mario Perez", "16 Soles", "24"));
+		//System.out.println("CLIENTE: C0001 ");
+		logger.debug("Consulta Rutas: " + lista.size());
+		return lista; 
+	}
+	
+	
+
+		@GET 
+		@Path("/listar/{destino}") 
+		@Produces("text/plain") 
+		public String listar(@PathParam("destino") String destino) { 
+			logger.debug("Lista Ruta: " + destino);
+		        return destino; 
+		} 
+
+	 
+
+}
